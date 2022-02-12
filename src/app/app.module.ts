e435468ef544829be0a6b7c12f505caac574a4aa
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-02-12 20:33:24 +0300

    add e404 component

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { E404Component } from './e404/e404.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    E404Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
