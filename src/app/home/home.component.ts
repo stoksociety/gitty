71f2949e888898d642809b5f9d8b98cc1070be20
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-02-12 21:14:05 +0300

    add the info class

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = 'Git_App';
+  imageSrc = 'assets/joker.jpeg'
+  imageAlt = 'The Joker'
+  results!: any
+  p = '*click button again to confirm search*'
 
-  constructor() { }
+  constructor(private git: GithubRequestService) { }
 
   ngOnInit(): void {
   }
 
 }
+
+repo!: Repos
+
+doSearch(term: string) {
+  this.git.search(term);
+  this.results = this.git.results
+  this.git.search(`${term}/repos`)
+  this.repo = this.git.repos
+  console.log(this.results)
+}
+onSubmission() { }
+
+
+ngOnInit(): void {
+  this.repo = new Repos(0)
+}
+
---
+++ src/app/info-class.spec.ts
@@ -0,0 +1,7 @@
+import { InfoClass } from './info-class';
+
+describe('InfoClass', () => {
+  it('should create an instance', () => {
+    expect(new InfoClass()).toBeTruthy();
+  });
+});
