fc4b231d467d4357ef5c3d8ec3e83de318f8a1d3
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-02-12 21:15:05 +0300

    add the nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { E404Component } from './e404/e404.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     E404Component,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
